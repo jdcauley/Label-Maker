57e60c3fe3050c020102845c50fdefd3341048ec
Jordan Cauley <jordan@cauley.co>
2014-07-05 20:31:20 -0400

    Create labelmaker.js

---
+++ labelmaker.js
@@ -0,0 +1,35 @@
+var target = 'payment-input';
+
+  function onInput() {
+    var activeInput = document.activeElement;
+    var theParent = activeInput.parentNode.childElementCount;
+    if(activeInput && theParent < 2){
+      var title = activeInput.getAttribute('placeholder');
+      var inputLabel = document.createElement('label');
+      inputLabel.innerHTML = title;
+      activeInput.parentNode.insertBefore(inputLabel, activeInput);
+    }
+
+  }
+
+  function isChanged() {
+    var theInput = this;
+    console.log(theInput);
+    this.className = this.className + " input-done";
+    var theParent = this.parentNode;
+    var theChildren = this.parentNode.children;
+    for (var i = 0; i < theChildren.length; i++) {
+      var child = theChildren[i].tagName;
+      if(child === 'LABEL'){
+        theParent.removeChild(theChildren[i]);
+      }
+    }
+  }
+
+  var inputs = document.getElementsByClassName(target);
+  if (inputs) {
+    for (var i = 0; i < inputs.length; i++){
+      inputs[i].addEventListener("input", onInput, false);
+      inputs[i].addEventListener("change", isChanged, false);
+    }
+  }
